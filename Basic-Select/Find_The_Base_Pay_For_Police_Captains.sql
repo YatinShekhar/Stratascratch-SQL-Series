2f6cf9246f7fcd0fc30477e5ed1dc7a4b23053f1
Yatin Shekhar Rikhari <121398971+YatinShekhar@users.noreply.github.com>
2024-03-06 11:45:10 +0530

    Add files via upload

---
+++ Basic-Select/Find_The_Base_Pay_For_Police_Captains.sql
@@ -0,0 +1,5 @@
+-- https://platform.stratascratch.com/coding/9972-find-the-base-pay-for-police-captains?code_type=3
+
+select employeename, basepay 
+from sf_public_salaries
+where jobtitle = 'CAPTAIN III (POLICE DEPARTMENT)'
